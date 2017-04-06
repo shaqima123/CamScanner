c5924a0aa32796b8f30046f7bba0a0bbecf2de64
沙琪玛 <735042473@qq.com>
2017-04-06 16:24:36 +0800

    【Feature】搭建opencv环境框架，解决一些小bug

---
+++ CamScanner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CamScanner
+//
+//  Created by 沙琪玛 on 2017/4/6.
+//  Copyright © 2017年 沙琪玛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CamScanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CamScanner
+//
+//  Created by 沙琪玛 on 2017/4/6.
+//  Copyright © 2017年 沙琪玛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
